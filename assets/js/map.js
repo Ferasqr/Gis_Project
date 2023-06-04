7a08bdf4184e5fa8592759ddb281a74d07733b29
Sri603 <127546247+Sri603@users.noreply.github.com>
2023-06-04 23:26:50 +0200

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -194,9 +194,30 @@ document.addEventListener('DOMContentLoaded', function () {
             attributions: 'Tiles courtesy of <a href="https://carto.com/">Carto</a>'
         })
     });
+    //Bing Basemaps
+    var BING_MAPS_KEY = "cdSuP79ZvtPNL7cArZul~4HbLke3RP3rG7VKWs9RLUw~AhEyFJPxWCzeFokp2N7LV4mjGWEHGOWjyoJIlnayrAKvc4NYFYKn8OAHOLMs30rM";
+    var bingRoads = new ol.layer.Tile({
+        title: 'Bing Maps—Roads',
+        type: 'base',
+        visible: false,
+        source: new ol.source.BingMaps({
+            key: BING_MAPS_KEY,
+            imagerySet: 'Road'
+        })
+    });
+    var bingAerial = new ol.layer.Tile({
+        title: 'Bing Maps—Aerial',
+        type: 'base',
+        visible: false,
+        source: new ol.source.BingMaps({
+            key: BING_MAPS_KEY,
+            imagerySet: 'Aerial'
+        })
+    });
+
 
     //Get the list of basemaps and Extend the list using the .extend() function adding the 2 new layers
-    basemapLayers.getLayers().extend([stamenWatercolor, darkLayer, lightLayer]);
+    basemapLayers.getLayers().extend([stamenWatercolor, darkLayer, lightLayer, bingAerial, bingRoads]);
     //Add the layer switcher control
     var layerSwitcher = new ol.control.LayerSwitcher({});
     map.addControl(layerSwitcher);
